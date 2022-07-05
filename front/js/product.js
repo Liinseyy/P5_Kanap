8bdf6a00f9cb2b003544a114387cb389435093c7
Liinseyy <linsey20@live.fr>
2022-07-05 17:47:21 +0200

    update page product.js, error 404

--- front/js/product.js
+++ front/js/product.js
@@ -1,39 +1,15 @@
-const str = window.location;
-const url = new URL(str);
-//console.log(url); -> donne les infos url de la page selon l'id
-const id = url.searchParams.get("id");
-//console.log(id); -> attrape l'id de l'article
-const host = "http://localhost:3000/api/products";
-const objectURL = host + "api/products/" + id;
-//console.log(objectURL); -> donne lien page product de l'article
+const Product = window.location.search.split("?").join("");
+// me retourne l'id de l'article sans le "?"
+console.log(Product);
 
-let cardsFetch = function () {
-    fetch('http://localhost:3000/api/products')
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
+let productData =[];
 
+const fetchProduct = async () => {
+    fetch("http://localhost:3000/api/products/"+Product)  
+    .then((res) => res.json())
+    .then((promise) => {
+        console.log(promise);
+    });
+};
 
-        // Image
-        const img = document.querySelector(".item__img");
-        img.innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
-        
-        // Nom et titre
-        const name = document.getElementById("title");
-        name.innerHTML = data.name;
-        const title = document.querySelector("title");
-        title.innerHTML = data.name;
-        // Prix
-        const price = document.getElementById("price");
-        price.innerHTML = `${data.price}`;
-        // Description
-        const description = document.getElementById("description");
-        description.innerHTML = data.description;
-        // Couleurs
-        const color = document.getElementById("colors");
-        for (i = 0; i < data.colors.length; i++) {
-          color.innerHTML += `<option value="${data.colors[i]}">${data.colors[i]}</option>`;
-        }
-      });
-  };
-  cardsFetch();
+fetchProduct();
