d40844eb7c256ec10c30a6d808999c4817a124e1
Liinseyy <linsey20@live.fr>
2022-07-06 14:47:23 +0200

    fetch op, error 404 correct, starting display products

--- front/js/product.js
+++ front/js/product.js
@@ -1,15 +1,30 @@
-const Product = window.location.search.split("?").join("");
-// me retourne l'id de l'article sans le "?"
-console.log(Product);
+//Obtient l'url de la page product
+function getURL(){
+    return window.location.href;
+}
+//Obtient l'ID de l'article précis
+function getProductId(){
+    let url = new URL(getURL());
+    let productId = url.searchParams.get("id");
+    return productId;
+}
 
-let productData =[];
+let prod_id = getProductId();
 
+    let productData =[];
+    
 const fetchProduct = async () => {
-    fetch("http://localhost:3000/api/products/"+Product)  
+    fetch("http://localhost:3000/api/products/"+prod_id)  
     .then((res) => res.json())
     .then((promise) => {
         console.log(promise);
+        productData = promise
+        console.log(productData);
     });
-};
+    };
 
-fetchProduct();
+    const productDisplay = async () => {
+        await fetchProduct();
+    }
+
+    productDisplay();
