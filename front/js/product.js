5c274d5ac66ba10694e44fc3f8551764d4c18b59
Liinseyy <linsey20@live.fr>
2022-07-06 16:49:34 +0200

    innerHTML, error 404 with the elements (productData?)

--- front/js/product.js
+++ front/js/product.js
@@ -11,20 +11,29 @@ function getProductId(){
 
 let prod_id = getProductId();
 
-    let productData =[];
+let productData =[];    
     
 const fetchProduct = async () => {
     fetch("http://localhost:3000/api/products/"+prod_id)  
     .then((res) => res.json())
     .then((promise) => {
         console.log(promise);
-        productData = promise
-        console.log(productData);
+        productData = promise;
+        //console.log(productData);
     });
     };
 
-    const productDisplay = async () => {
+        let productDisplay = async () => {
         await fetchProduct();
-    }
+        
+        let img = document.querySelector(".item__img");
+        img.innerHTML = `<img src="${productData.imageUrl}" alt="${productData.altTxt}">`;  
+
+        let name = document.getElementById("title");
+        name.innerHTML = productData.name;
+    };
 
     productDisplay();
+
+
+       
